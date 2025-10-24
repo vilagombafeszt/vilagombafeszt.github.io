79c75092aa957453df9efd84441a93edb8f9db8c
Kovács Donát <kovacsdonat@edu.bme.hu>
2025-10-24 20:55:32 +0200

    fix safari scrolling issue

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    cookieconsent.run({
+      notice_banner_type: "headline",
+      consent_type: "express",
+      palette: "dark",
+      language: "hu",
+      page_load_consent_levels: ["strictly-necessary"],
+      notice_banner_reject_button_hide: false,
+      preferences_center_close_button_hide: false,
+      page_refresh_confirmation_buttons: false,
+      website_name: "ViláGomba Feszt"
+    });
+
     // Add passive touch event listener to improve scrolling performance
     document.addEventListener('touchstart', function(){}, {passive: true});
     document.addEventListener('touchmove', function(){}, {passive: true});
