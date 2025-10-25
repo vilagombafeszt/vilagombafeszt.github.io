906e9ce20ea6fa7b2b5ed23fc2882976c612f033
Kovács Donát <kovacsdonat@edu.bme.hu>
2025-10-25 19:41:39 +0200

    faszomat a safariba

--- scripts.js
+++ scripts.js
@@ -1,24 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-    cookieconsent.run({
-      notice_banner_type: "headline",
-      consent_type: "express",
-      palette: "dark",
-      language: "hu",
-      page_load_consent_levels: ["strictly-necessary"],
-      notice_banner_reject_button_hide: false,
-      preferences_center_close_button_hide: false,
-      page_refresh_confirmation_buttons: false,
-      website_name: "ViláGomba Feszt"
-    });
-
-    // Add passive touch event listener to improve scrolling performance
-    document.addEventListener('touchstart', function(){}, {passive: true});
-    document.addEventListener('touchmove', function(){}, {passive: true});
-
-    document.body.style.overflow = 'auto';
-    document.documentElement.style.overflow = 'auto';
-
     // Adjusts the href attributes based on window width
     $(window).resize(function() {
         if ($(window).width() >= 700) {
