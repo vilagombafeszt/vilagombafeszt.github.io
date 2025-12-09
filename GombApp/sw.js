66e37918fa9b7619722d0f2e13e044c29e9fdbeb
Kovács Donát <kovacsdonat@edu.bme.hu>
2025-12-09 21:58:21 +0100

    Update button margin for improved layout

--- GombApp/sw.js
+++ GombApp/sw.js
@@ -1,5 +1,5 @@
 // Service Worker for GombApp PWA
-const CACHE_NAME = 'gombapp-v1.1.4';
+const CACHE_NAME = 'gombapp-v1.1.4alpha';
 
 // Static assets to cache on install
 // Note: Icon files (icon-*.png) are not cached as they are placeholders
